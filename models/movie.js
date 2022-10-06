96d11eaece24fa2dc8cc8b550d2907aeca60880c
Jim Clark <jim.clark@generalassemb.ly>
2022-10-06 15:10:42 -0700

    Add reviewSchema

--- models/movie.js
+++ models/movie.js
@@ -2,6 +2,21 @@ const mongoose = require('mongoose');
 // Shortcut to the mongoose.Schema class
 const Schema = mongoose.Schema;
 
+const reviewSchema = new Schema({
+  content: {
+    type: String,
+    required: true
+  },
+  rating: {
+    type: Number,
+    min: 1,
+    max: 5,
+    default: 5
+  }
+}, {
+  timestamps: true
+});
+
 const movieSchema = new Schema({
   title: {
     type: String,
@@ -19,7 +34,8 @@ const movieSchema = new Schema({
     enum: ['G', 'PG', 'PG-13', 'R']
   },
   cast: [String],
-  nowShowing: {type: Boolean, default: false}
+  nowShowing: {type: Boolean, default: false},
+  reviews: [reviewSchema]
 }, {
   timestamps: true
 });
