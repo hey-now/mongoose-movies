e02087f2dae14d77acadd05ac98de5891ef1164c
Jim Clark <jim.clark@generalassemb.ly>
2022-10-06 14:17:25 -0700

    Embedded lesson starter code

--- controllers/movies.js
+++ controllers/movies.js
@@ -2,22 +2,35 @@ const Movie = require('../models/movie');
 
 module.exports = {
   index,
+  show,
   new: newMovie,
   create
 };
 
 function index(req, res) {
   Movie.find({}, function(err, movies) {
-    res.render('movies/index', { movies });
+    res.render('movies/index', { title: 'All Movies', movies });
   });
 }
 
+function show(req, res) {
+  Movie.findById(req.params.id, function(err, movie) {
+    res.render('movies/show', { title: 'Movie Detail', movie });
+  });
+}
+
+function newMovie(req, res) {
+  res.render('movies/new', { title: 'Add Movie' });
+}
+
 function create(req, res) {
-  // Convert nowShowing's checkbox to a boolean
+  // Convert nowShowing's checkbox of nothing or "on" to boolean
   req.body.nowShowing = !!req.body.nowShowing;
   // Remove leading/trailing spaces
   req.body.cast = req.body.cast.trim();
+  // Split if it's not an empty string
   if (req.body.cast) req.body.cast = req.body.cast.split(/\s*,\s*/);
+  // Delete empty properties on req.body for defaults to happen 
   for (let key in req.body) {
     if (req.body[key] === '') delete req.body[key];
   }
@@ -27,8 +40,4 @@ function create(req, res) {
     console.log(movie);
     res.redirect('/movies');
   });
-}
-
-function newMovie(req, res) {
-  res.render('movies/new');
 }
--- routes/movies.js
+++ routes/movies.js
@@ -4,11 +4,13 @@ var moviesCtrl = require('../controllers/movies');
 
 // All routes start with '/movies'
 
-// GET /movies
+// GET /movies (index functionality)
 router.get('/', moviesCtrl.index);
-// GET /movies/new
+// GET /movies/new (new functionalit)
 router.get('/new', moviesCtrl.new);
-// POST /movies
+// GET /movies/:id (show functionality)
+router.get('/:id', moviesCtrl.show);
+// POST /movies (create functionality)
 router.post('/', moviesCtrl.create);
 
 module.exports = router;
